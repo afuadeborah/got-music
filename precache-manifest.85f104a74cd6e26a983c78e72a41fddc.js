873bfb41af48e4aa22d03680fb905186ea2e32a1
Afua <afrimpong.15@gmail.com>
2020-06-29 15:42:50 -0400

    Updates

---
+++ precache-manifest.85f104a74cd6e26a983c78e72a41fddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adb3b608ab13c42831ead135e8a6727",
+    "url": "/afuadeborah/got-music/index.html"
+  },
+  {
+    "revision": "aa5850719c347545a165",
+    "url": "/afuadeborah/got-music/static/css/main.b5dab23c.chunk.css"
+  },
+  {
+    "revision": "e2b9b507c64d7fd1b024",
+    "url": "/afuadeborah/got-music/static/js/2.a6b64894.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "/afuadeborah/got-music/static/js/2.a6b64894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5850719c347545a165",
+    "url": "/afuadeborah/got-music/static/js/main.fd9282e2.chunk.js"
+  },
+  {
+    "revision": "e7e1fced60ac54cbfbee",
+    "url": "/afuadeborah/got-music/static/js/runtime-main.22d637da.js"
+  }
+]);
