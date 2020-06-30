30d73cc236bf6c9975aa887a7ef3c8e94d343489
Afua <afrimpong.15@gmail.com>
2020-06-29 20:03:29 -0400

    Updates

---
+++ precache-manifest.b9e2679433f6d38986a1a521847b2227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1effd8d653cdcf4f3902ed5396176673",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cdd290202a2c10b5268",
+    "url": "./static/css/main.b5dab23c.chunk.css"
+  },
+  {
+    "revision": "71c70007bbdf73afda15",
+    "url": "./static/js/2.8a594a2f.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "./static/js/2.8a594a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cdd290202a2c10b5268",
+    "url": "./static/js/main.f4d3c539.chunk.js"
+  },
+  {
+    "revision": "fe1ce0e98826f48a4969",
+    "url": "./static/js/runtime-main.720e790e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/afuadeborah/got-music/precache-manifest.7f57fd22649d31fcf1243312fea3d6d4.js"
+  "./precache-manifest.b9e2679433f6d38986a1a521847b2227.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/afuadeborah/got-music/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
